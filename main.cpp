e2dcc9513f2c5018aa3d7e68cfe2c3e60fb0c761
aiden cox <cox.378@wright.edu>
2025-03-08 16:03:48 -0500

    gmae board print out

--- main.cpp
+++ main.cpp
@@ -4,12 +4,29 @@
 void drawBoard (char *spaces);
 void playerMove(char *spaces, char player);
 void computerMove(char *spaces, char computer);
-void checkWinner(char *spaces, char player, char computer);
-void checkTie(char *spaces);
+bool checkWinner(char *spaces, char player, char computer);
+bool checkTie(char *spaces);
 int main() {
+
+    char spaces[9] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+    char player = 'X';
+    char computer = 'O';
+    bool running = true;
+
+
+    drawBoard(spaces);
     return 0;
 }
 void drawBoard (char *spaces) {
+    std::cout << "     |      |     " << '\n';
+    std::cout << "  " << spaces[0] <<  "  |  " << spaces [1]  << "   |  " << spaces[2] <<  "   "  << '\n';
+    std::cout << "_____|______|______" << '\n';
+    std::cout << "     |      |     " << '\n';
+    std::cout << "  " << spaces[3] <<  "  |  " << spaces [4]  << "   |  " << spaces[5] <<  "   "  << '\n';
+    std::cout << "_____|______|______" << '\n';
+    std::cout << "     |      |     " << '\n';
+    std::cout << "  " << spaces[6] <<  "  |  " << spaces [7]  << "   |  " << spaces[8] <<  "   "  << '\n';
+
 
 }
 void playerMove(char *spaces, char player) {
@@ -18,9 +35,10 @@ void playerMove(char *spaces, char player) {
 void computerMove(char *spaces, char computer) {
 
 }
-void checkWinner(char *spaces, char player, char computer) {
+bool checkWinner(char *spaces, char player, char computer) {
 
+    return 0;
 }
-void checkTie(char *spaces) {
-    
+bool checkTie(char *spaces) {
+    return 0;
 }
