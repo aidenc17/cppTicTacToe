648d79dc3a31e588ac4ec694eccdf5e3c4441131
aiden cox <cox.378@wright.edu>
2025-03-08 15:48:48 -0500

    Initial commit and layout setup

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <ctime>
+
+void drawBoard (char *spaces);
+void playerMove(char *spaces, char player);
+void computerMove(char *spaces, char computer);
+void checkWinner(char *spaces, char player, char computer);
+void checkTie(char *spaces);
+int main() {
+    return 0;
+}
+void drawBoard (char *spaces) {
+
+}
+void playerMove(char *spaces, char player) {
+
+}
+void computerMove(char *spaces, char computer) {
+
+}
+void checkWinner(char *spaces, char player, char computer) {
+
+}
+void checkTie(char *spaces) {
+    
+}
