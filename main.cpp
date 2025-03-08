cc53c71393f1f2a0e53ea2f7aa2b2e0406da4a4d
aiden cox <cox.378@wright.edu>
2025-03-08 16:11:43 -0500

    player move func

--- main.cpp
+++ main.cpp
@@ -15,9 +15,16 @@ int main() {
 
 
     drawBoard(spaces);
+
+    while (running) {
+        playerMove(spaces, player);
+        drawBoard(spaces);
+        computerMove(spaces, computer);
+    }
     return 0;
 }
 void drawBoard (char *spaces) {
+    std::cout << '\n';
     std::cout << "     |      |     " << '\n';
     std::cout << "  " << spaces[0] <<  "  |  " << spaces [1]  << "   |  " << spaces[2] <<  "   "  << '\n';
     std::cout << "_____|______|______" << '\n';
@@ -26,11 +33,23 @@ void drawBoard (char *spaces) {
     std::cout << "_____|______|______" << '\n';
     std::cout << "     |      |     " << '\n';
     std::cout << "  " << spaces[6] <<  "  |  " << spaces [7]  << "   |  " << spaces[8] <<  "   "  << '\n';
+    std::cout << "     |      |     " << '\n';
+    std::cout << '\n';
 
 
 }
 void playerMove(char *spaces, char player) {
+    int number;
+    do {
+        std::cout << "Enter a spot to place a marker (1-9): ";
+        std::cin >> number;
+        number--;
+        if (spaces[number] == ' ') {
+            spaces[number] = player;
+            break;
+        }
 
+    }while (!number > 0 || !number < 8);
 }
 void computerMove(char *spaces, char computer) {
 
